9c57f444c717a466d21cb6566efe21912f39f3dd
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2025-06-25 11:27:13 +0300

    added swagger schema

--- timetable/core/serializers/audience_serializer.py
+++ timetable/core/serializers/audience_serializer.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from timetable.core.models import Audience
@@ -11,6 +12,7 @@ class AudienceWithSubjectsSerializer(serializers.ModelSerializer):
         model = Audience
         fields = ("id", "name", "subjects")
 
+    @extend_schema_field(SubjectSerializer(many=True))
     def get_subjects(self, obj):
         subjects = getattr(obj, "subject_set", []).all()
         return SubjectSerializer(subjects, many=True).data
