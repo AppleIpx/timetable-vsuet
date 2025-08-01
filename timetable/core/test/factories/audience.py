70532b8a7bba16cbd529af0427ee511e93031b2d
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2025-08-01 13:01:57 +0300

    fix: fixed error in the test, filtering items

--- timetable/core/test/factories/audience.py
+++ timetable/core/test/factories/audience.py
@@ -1,11 +1,11 @@
-from factory import Faker
+import factory
 from factory.django import DjangoModelFactory
 
 from timetable.core.models import Audience
 
 
 class AudienceFactory(DjangoModelFactory):
-    name = Faker("word")
+    name = factory.Sequence(lambda n: f"Аудитория-{n}")
 
     class Meta:
         model = Audience
